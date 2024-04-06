333d848699b66fc160deeaafd5554b84035e220d
BERADQ <adqber123@outlook.com>
2024-04-07 00:40:43 +0800

    add something

--- src/chat.ts
+++ src/chat.ts
@@ -1,10 +1,9 @@
-export const DefaultApi = "http://chatgpt.skyytor.club:2800/v1/chat/completions"
 export interface ChatRequest {
-  model: string,
-  messages: Message[],
-  stream?: boolean
+  model: string;
+  messages: Message[];
+  stream?: boolean;
 }
 export interface Message {
-  role: "user" | "system" | "assistant",
-  content: string
+  role: "user" | "system" | "assistant";
+  content: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Context, Logger, Schema } from "koishi";
-import { ChatRequest, DefaultApi, Message } from "./chat";
+import { ChatRequest,  Message } from "./chat";
+import { DefaultApi } from "./defaultapi";
 import { Liquid } from "liquidjs";
 import fs from "node:fs";
 import path from "node:path";
@@ -8,46 +9,76 @@ export const name = "sus-chat";
 
 export interface Config {
   api: string;
+  api_key: string;
   max_length: number;
   prompt_directory: string;
 }
 
 export const Config: Schema<Config> = Schema.object({
-  api: Schema.string().default(DefaultApi),
+  api: Schema.string().default(DefaultApi.url),
+  api_key: Schema.string().default(DefaultApi.key),
   max_length: Schema.number().default(10).step(1),
   prompt_directory: Schema.path().default("./sus-chat"),
 });
 
 const logger = new Logger("sus-chat");
-export async function apply(ctx: Context, config: Config) {
+export function apply(ctx: Context, config: Config) {
   const engine = new Liquid();
   let prompts = new Prompts(ctx, config.prompt_directory, engine);
-  await prompts.init();
-  ctx.command("sus <content:text>", "可疑").action(async (_, content) => {
+  ctx.command("sus <content:text>", "可疑").action(async (s, content) => {
     let node: Message = { role: "user", content };
     let req: ChatRequest = {
       model: "gpt-3.5-turbo",
-      messages: [...prompts.get("hello"), node],
+      messages: [
+        ...prompts.get("hello", {
+          session: JSON.parse(JSON.stringify(s.session)),
+        }),
+        node,
+      ],
       stream: false,
     };
-    let res = await ctx.http.post(config.api, req, { responseType: "json" });
+    let res = await ctx.http.post(config.api, req, {
+      responseType: "json",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${config.api_key}`,
+      },
+    });
     return res.choices[0].message.content;
+  });
+  ctx.command("sus.prom").action(async (s) => {
+    s.session.channel;
+    return JSON.stringify(
+      prompts.get("hello", {
+        session: JSON.parse(JSON.stringify(s.session)),
+      })
+    );
+  });
+  ctx.command("sus.reload").action(() => {
+    prompts = new Prompts(ctx, config.prompt_directory, engine);
+    return "重载完成";
   });
 }
 class Prompts {
-  init_file_path: string;
   prompts_map: { [key: string]: Message[] } = {};
-  init_object: any = {};
   liquid: Liquid;
   constructor(ctx: Context, directory: string, liquid: Liquid) {
-    this.init_file_path = path.join(directory, "init.js");
+    let init_file_path = path.join(directory, "init.js");
+    if (fs.existsSync(init_file_path)) {
+      liquid.plugin((Liquid) => {
+        let fun: (
+          this: Liquid,
+          arg0: Context,
+          arg1: typeof Liquid
+        ) => void = require(init_file_path);
+        fun.bind(this)(ctx, Liquid);
+      });
+    }
     let files = fs
       .readdirSync(directory)
       .filter((file) => file.toLowerCase().endsWith(".yml"));
     let prompts: { name: string; content: Message[] }[] = files.map((file) => {
       let name = path.parse(file).name;
-      console.log(file);
-
       let content_string = fs.readFileSync(path.join(directory, file), "utf-8");
       let content = YAML.parse(content_string);
       return { name, content };
@@ -57,25 +88,13 @@ class Prompts {
     });
     this.liquid = liquid;
   }
-  async init() {
-    if (fs.existsSync(this.init_file_path)) {
-      try {
-        this.init_object = await import(this.init_file_path);
-      } catch (e) {
-        logger.error(e);
-      }
-    }
-  }
   get(name: string, scope: { [key: string]: any }): Message[] {
     if (!this.prompts_map[name]) {
       throw "prompt not found";
     }
     let temp = this.prompts_map[name];
     let messages: Message[] = temp.map((message) => {
-      const content = this.liquid.parseAndRenderSync(message.content, {
-        ...scope,
-        ...this.init_object,
-      });
+      const content = this.liquid.parseAndRenderSync(message.content, scope);
       return { role: message.role, content };
     });
     return messages;
