15163c451c796578097052820b27dac767c18f5a
BERADQ <adqber123@outlook.com>
2024-04-08 03:22:48 +0800

    pub

--- src/chat.ts
+++ src/chat.ts
@@ -1,9 +1,221 @@
+import fs from "node:fs";
+import { Liquid, Template } from "liquidjs";
+import { Context, Session } from "koishi";
+import path from "node:path";
+import YAML from "yaml";
 export interface ChatRequest {
   model: string;
   messages: Message[];
   stream?: boolean;
 }
+type Role = "user" | "system" | "assistant";
 export interface Message {
-  role: "user" | "system" | "assistant";
+  role: Role;
   content: string;
+}
+
+export interface PromptsFile {
+  name: string;
+  prompts: Message[];
+  postprocessing?: string;
+}
+export interface PromptsFileReal {
+  name: string;
+  prompts: { role: Role; content: Template[] }[];
+  postprocessing?: Template[];
+}
+export interface PromptsReal {
+  postprocessing: (message: Message) => Message;
+  prompts: Message[];
+}
+export class Prompts {
+  prompts_map: {
+    [key: string]: PromptsFileReal;
+  } = {};
+  liquid: Liquid;
+  reload(ctx: Context, directory: string) {
+    this.constructor.call(this, ctx, directory);
+  }
+  constructor(ctx: Context, directory: string) {
+    const liquid = new Liquid();
+    const init_file_path = path.join(directory, "init.js");
+    if (fs.existsSync(init_file_path)) {
+      liquid.plugin((Liquid) => {
+        let fun: (
+          this: Liquid,
+          arg0: Context,
+          arg1: typeof Liquid
+        ) => void = require(init_file_path);
+        fun.bind(this)(ctx, Liquid);
+      });
+    }
+    let files = fs
+      .readdirSync(directory)
+      .filter((file) => file.toLowerCase().endsWith(".yml"));
+    let prompts: PromptsFile[] = files.map((file) => {
+      const content_string = fs.readFileSync(
+        path.join(directory, file),
+        "utf-8"
+      );
+      const content: PromptsFile = YAML.parse(content_string);
+      return content;
+    });
+    prompts.forEach((prompt) => {
+      const prompts: { role: Role; content: Template[] }[] = prompt.prompts.map(
+        (prompt) => {
+          return { role: prompt.role, content: liquid.parse(prompt.content) };
+        }
+      );
+      const postprocessing = prompt.postprocessing
+        ? liquid.parse(prompt.postprocessing)
+        : undefined;
+      this.prompts_map[prompt.name] = {
+        name: prompt.name,
+        prompts,
+        postprocessing,
+      };
+    });
+    this.liquid = liquid;
+  }
+  get names(): string[] {
+    return Object.keys(this.prompts_map);
+  }
+  get(name: string, scope: { [key: string]: any }): PromptsReal {
+    if (!this.prompts_map[name]) {
+      throw "prompt not found";
+    }
+    const temp = this.prompts_map[name];
+    const messages: Message[] = temp.prompts.map((message) => {
+      const content = this.liquid.renderSync(message.content, scope);
+      return { role: message.role, content };
+    });
+    let postprocessing;
+    if (temp.postprocessing) {
+      postprocessing = (message: Message) => {
+        const content = this.liquid.render(temp.postprocessing, {
+          message: message,
+          ...scope,
+        });
+        return { role: message.role, content };
+      };
+    } else {
+      postprocessing = (message: Message) => message;
+    }
+    return { prompts: messages, postprocessing };
+  }
+}
+export interface ChatConfig {
+  max_length: number;
+}
+export class ChatServer {
+  prompts?: Prompts | undefined;
+  prompt_str?: Template[] | undefined;
+  #liquid?: Liquid | undefined;
+  api: { url: string; key: string };
+  #recollect: { [cid: string]: { [prompt_name: string]: Message[] } };
+  max_length: number;
+  get liquid() {
+    return this.#liquid ?? this.prompts.liquid;
+  }
+  get recollect() {
+    return this.#recollect;
+  }
+  set recollect(value) {
+    this.#recollect = value;
+    this.#limit_length();
+  }
+  #limit_length() {
+    for (const cid in this.#recollect) {
+      for (const prompt_name in this.#recollect[cid]) {
+        while (this.#recollect[cid][prompt_name].length > this.max_length) {
+          this.#recollect[cid][prompt_name].shift();
+        }
+      }
+    }
+  }
+  update_recollect(
+    cid: string,
+    prompt_name: string,
+    callback: (messages: Message[]) => Message[]
+  ) {
+    if (!this.#recollect[cid]) {
+      this.#recollect[cid] = {};
+    }
+    if (!this.#recollect[cid][prompt_name]) {
+      this.#recollect[cid][prompt_name] = [];
+    }
+    this.#recollect[cid][prompt_name] = callback(
+      this.#recollect[cid][prompt_name]
+    );
+    this.#limit_length();
+  }
+  constructor(
+    apiurl: string,
+    apikey: string,
+    config: ChatConfig,
+    prompts: Prompts | string
+  ) {
+    this.#recollect = {};
+    this.max_length = config.max_length;
+    if (typeof prompts === "string") {
+      this.#liquid = new Liquid();
+      this.prompt_str = this.#liquid.parse(prompts);
+    } else {
+      this.prompts = prompts;
+    }
+    this.api = { url: apiurl, key: apikey };
+  }
+  async chat(
+    message: Message,
+    prompt_name: string,
+    ctx: Context,
+    session: Session
+  ): Promise<Message> {
+    prompt_name = prompt_name ?? "#";
+    if (typeof this.recollect[session.cid] === "undefined") {
+      this.recollect[session.cid] = {};
+    }
+    if (typeof this.recollect[session.cid][prompt_name] === "undefined") {
+      this.recollect[session.cid][prompt_name] = [];
+    }
+    const recall = this.recollect[session.cid][prompt_name];
+    let prompt_real: PromptsReal;
+    if (typeof this.prompts === "undefined") {
+      prompt_real = {
+        prompts: [
+          {
+            role: "system",
+            content: await this.#liquid.render(this.prompt_str, {
+              session: JSON.parse(JSON.stringify(session)),
+            }),
+          },
+        ],
+        postprocessing: (message) => message,
+      };
+    } else {
+      prompt_real = this.prompts.get(prompt_name, {
+        session: JSON.parse(JSON.stringify(session)),
+      });
+    }
+    const my_message = prompt_real.postprocessing(message);
+    const req: ChatRequest = {
+      model: "gpt-3.5-turbo",
+      messages: [...recall, ...prompt_real.prompts, my_message],
+      stream: false,
+    };
+    const res = await ctx.http.post(this.api.url, req, {
+      responseType: "json",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.api.key}`,
+      },
+    });
+    const result = prompt_real.postprocessing(res.choices[0].message);
+    this.update_recollect(session.cid, prompt_name, (messages) => {
+      messages.push(my_message);
+      messages.push(result);
+      return messages;
+    });
+    return result;
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,102 +1,98 @@
 import { Context, Logger, Schema } from "koishi";
-import { ChatRequest,  Message } from "./chat";
+import { ChatServer, Message, Prompts } from "./chat";
 import { DefaultApi } from "./defaultapi";
-import { Liquid } from "liquidjs";
-import fs from "node:fs";
-import path from "node:path";
-import YAML from "yaml";
 export const name = "sus-chat";
 
 export interface Config {
   api: string;
   api_key: string;
   max_length: number;
-  prompt_directory: string;
+  prompt: {
+    pro_prompt: boolean;
+    prompt_str?: string;
+    prompt_directory?: string;
+    default_prompt?: string;
+  };
 }
 
 export const Config: Schema<Config> = Schema.object({
-  api: Schema.string().default(DefaultApi.url),
-  api_key: Schema.string().default(DefaultApi.key),
-  max_length: Schema.number().default(10).step(1),
-  prompt_directory: Schema.path().default("./sus-chat"),
+  api: Schema.string().default(DefaultApi.url).description("API"),
+  api_key: Schema.string().default(DefaultApi.key).description("KEY"),
+  max_length: Schema.number()
+    .role("slider")
+    .min(3)
+    .max(50)
+    .default(10)
+    .step(1)
+    .description("记忆长度上限"),
+  prompt: Schema.intersect([
+    Schema.object({
+      pro_prompt: Schema.boolean().default(false).description("使用专业提示词"),
+    }),
+    Schema.union([
+      Schema.object({
+        pro_prompt: Schema.const(false),
+        prompt_str: Schema.string()
+          .default(
+            "你是个有用的助理，当前与你对话的用户的昵称为:{{ session.user.name }}"
+          )
+          .description("提示词"),
+      }),
+      Schema.object({
+        pro_prompt: Schema.const(true).required(),
+        prompt_directory: Schema.path()
+          .default("./sus-chat")
+          .description("提示词文件所在目录"),
+        default_prompt: Schema.string()
+          .default("default")
+          .description("默认提示词"),
+      }),
+    ]),
+  ]),
 });
 
 const logger = new Logger("sus-chat");
 export function apply(ctx: Context, config: Config) {
-  const engine = new Liquid();
-  let prompts = new Prompts(ctx, config.prompt_directory, engine);
+  const server = new ChatServer(
+    config.api,
+    config.api_key,
+    { max_length: config.max_length },
+    config.prompt.pro_prompt
+      ? new Prompts(ctx, config.prompt.prompt_directory)
+      : config.prompt.prompt_str
+  );
   ctx.command("sus <content:text>", "可疑").action(async (s, content) => {
-    let node: Message = { role: "user", content };
-    let req: ChatRequest = {
-      model: "gpt-3.5-turbo",
-      messages: [
-        ...prompts.get("hello", {
-          session: JSON.parse(JSON.stringify(s.session)),
-        }),
-        node,
-      ],
-      stream: false,
-    };
-    let res = await ctx.http.post(config.api, req, {
-      responseType: "json",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${config.api_key}`,
-      },
-    });
-    return res.choices[0].message.content;
+    const node: Message = { role: "user", content };
+    const result = await server.chat(
+      node,
+      config.prompt.default_prompt,
+      ctx,
+      s.session
+    );
+    return result.content;
   });
-  ctx.command("sus.prom").action(async (s) => {
-    s.session.channel;
+  ctx.command("sus.prom [name:string]").action(async (s, name) => {
+    if (!config.prompt.pro_prompt) {
+      return server.liquid.render(server.prompt_str, {
+        session: JSON.parse(JSON.stringify(s.session)),
+      });
+    }
+    if (!name) {
+      return server.prompts.names.join("\n");
+    }
     return JSON.stringify(
-      prompts.get("hello", {
+      server.prompts.get(name, {
         session: JSON.parse(JSON.stringify(s.session)),
       })
     );
   });
-  ctx.command("sus.reload").action(() => {
-    prompts = new Prompts(ctx, config.prompt_directory, engine);
-    return "重载完成";
+  ctx.command("sus.eval <content:text>").action(async (s, content) => {
+    const result = await server.liquid.parseAndRender(content, {
+      session: JSON.parse(JSON.stringify(s.session)),
+    });
+    return result;
   });
-}
-class Prompts {
-  prompts_map: { [key: string]: Message[] } = {};
-  liquid: Liquid;
-  constructor(ctx: Context, directory: string, liquid: Liquid) {
-    let init_file_path = path.join(directory, "init.js");
-    if (fs.existsSync(init_file_path)) {
-      liquid.plugin((Liquid) => {
-        let fun: (
-          this: Liquid,
-          arg0: Context,
-          arg1: typeof Liquid
-        ) => void = require(init_file_path);
-        fun.bind(this)(ctx, Liquid);
-      });
-    }
-    let files = fs
-      .readdirSync(directory)
-      .filter((file) => file.toLowerCase().endsWith(".yml"));
-    let prompts: { name: string; content: Message[] }[] = files.map((file) => {
-      let name = path.parse(file).name;
-      let content_string = fs.readFileSync(path.join(directory, file), "utf-8");
-      let content = YAML.parse(content_string);
-      return { name, content };
-    });
-    prompts.forEach((prompt) => {
-      this.prompts_map[prompt.name] = prompt.content;
-    });
-    this.liquid = liquid;
-  }
-  get(name: string, scope: { [key: string]: any }): Message[] {
-    if (!this.prompts_map[name]) {
-      throw "prompt not found";
-    }
-    let temp = this.prompts_map[name];
-    let messages: Message[] = temp.map((message) => {
-      const content = this.liquid.parseAndRenderSync(message.content, scope);
-      return { role: message.role, content };
-    });
-    return messages;
-  }
+  ctx.command("sus.reload").action(() => {
+    server.prompts?.reload(ctx, config.prompt.prompt_directory);
+  });
 }
